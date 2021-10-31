f73ddaba6bd32ecc2fa01092af1c055ffdb211cb
Romil Shah <romilshah2199@gmail.com>
2021-10-31 11:17:37 +0530

    csrf-token

--- src/main/java/com/example/demo/security/AppSecurityConfig.java
+++ src/main/java/com/example/demo/security/AppSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -23,37 +24,26 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("foo")
                 .password(passwordEncoder.encode("foo"))
-                //.roles(ApplicationUserRole.STUDENT.name())
                 .authorities(ApplicationUserRole.STUDENT.getAuthorities())
                 .and()
                 .withUser("bar")
                 .password(passwordEncoder.encode("bar"))
-                //.roles(ApplicationUserRole.ADMIN.name())
                 .authorities(ApplicationUserRole.ADMIN.getAuthorities())
                 .and()
                 .withUser("tom")
                 .password(passwordEncoder.encode("tom"))
-//                .roles(ApplicationUserRole.ADMINTRAINEE.name());
                 .authorities(ApplicationUserRole.ADMINTRAINEE.getAuthorities());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
-                // using http with role based
-                /*.antMatchers(HttpMethod.POST, "/management/**").hasRole(ApplicationUserRole.ADMIN.name())
-                .antMatchers(HttpMethod.PUT, "/management/**").hasRole(ApplicationUserRole.ADMIN.name())
-                .antMatchers(HttpMethod.DELETE, "/management/**").hasRole(ApplicationUserRole.ADMIN.name())
-                .antMatchers(HttpMethod.GET, "/management/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name())*/
-                // using http with authority based
-                /*.antMatchers(HttpMethod.POST, "/management/**").hasAuthority(ApplicationUserPermission.STUDENT_WRITE.name())
-                .antMatchers(HttpMethod.PUT, "/management/**").hasAuthority(ApplicationUserPermission.STUDENT_WRITE.name())
-                .antMatchers(HttpMethod.DELETE, "/management/**").hasAuthority(ApplicationUserPermission.STUDENT_WRITE.name())
-                .antMatchers(HttpMethod.GET, "/management/**").hasAuthority(ApplicationUserPermission.STUDENT_READ.name())*/
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
